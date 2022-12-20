8fbcbecf728fa24cf28b78294aef7fc7f08c293f
JBroadway <jeremiah.broadway@hotmail.com>
2022-12-20 13:18:05 -0600

    improvements

--- scrap.lua
+++ scrap.lua
@@ -1,18 +1,37 @@
 :
-function shake(val, func)
-	amplitude 	= (((0.5+val) - (0.5-val))/2)
-	period		= (300*(2/300)^gui.Speed)
-	midpoint	= (((0.5 - val) + (0.5 + val)) / 2)
-    phase       = gui.Offset
+-- Returns value 'y' which turns input 'x' (from 0 to 1) into an exponential curve.
+function expCurve(x, slope, a, b)
+    slope 	= slope or 100
+	a		= a or 0
+	b		= b or 1
+    -- y 		= (slope^x -1)/(slope-1)
+	y		= a + (b-a)*((slope^x-1)/(slope-1))
+    return y
+end
+
+-- Shake Function
+function shake(x1, x2, phase, speed, func)
+	amplitude 	= ((x1 - x2) / 2)
+	-- period		= (300 * (2 / 300)^speed)
+	-- period		= expCurve(speed, .01, 300, 0.75)
+	period 		= expCurve(speed, .01, 200, 2.1)
+	midpoint	= ((x2 + x1) / 2)
+    phase       = phase or 1
 	
 	if func == "sin" then
 		shakeit	= amplitude * sin(2 * pi * time / period + pi/phase) + midpoint
-	else
+	elseif func == "cos" then
 		shakeit	= amplitude * cos(2 * pi * time / period) + midpoint
 	end
 	return shakeit
 end
 
-return Point(shake(gui.xDev, "sin"), shake(gui.yDev, "cos"))
+sinPhase 	= shake(1, 1 + gui.Offset/50, 1, 0.1, "sin")
 
+x1, y1 		= expCurve(gui.xDev), expCurve(gui.yDev)
+x2, y2 		= expCurve(gui.xDev), expCurve(gui.yDev)
 
+x1, y1 		= 0.5-x1, 0.5-y1
+x2, y2 		= 0.5+x2, 0.5+y2
+
+return Point(shake(1, 0, sinPhase, dev.a, "sin"), 0.5)
--- shake.lua
+++ shake.lua
@@ -1,7 +1,19 @@
 :
+-- Returns value 'y' which turns input 'x' (from 0 to 1) into an exponential curve.
+function expCurve(x, slope, a, b)
+    slope 	= slope or 100
+	a		= a or 0
+	b		= b or 1
+    -- y 		= (slope^x -1)/(slope-1)
+	y		= a + (b-a)*((slope^x-1)/(slope-1))
+    return y
+end
+
+-- Shake Function
 function shake(x1, x2, phase, speed, func)
 	amplitude 	= ((x1 - x2) / 2)
-	period		= (300 * (2 / 300)^speed)
+	-- period		= (300 * (2 / 300)^speed)
+	period		= expCurve(speed, .01, 100, 2.1)
 	midpoint	= ((x2 + x1) / 2)
     phase       = phase or 1
 	
@@ -14,8 +26,12 @@ function shake(x1, x2, phase, speed, func)
 end
 
 sinPhase 	= shake(1, 1 + gui.Offset/50, 1, 0.1, "sin")
-x1, y1 		= 0.5-gui.xDev, 0.5-gui.yDev
-x2, y2 		= 0.5+gui.xDev, 0.5+gui.yDev
+
+x1, y1 		= expCurve(gui.xDev), expCurve(gui.yDev)
+x2, y2 		= expCurve(gui.xDev), expCurve(gui.yDev)
+
+x1, y1 		= 0.5-x1, 0.5-y1
+x2, y2 		= 0.5+x2, 0.5+y2
 
 return Point(shake(x2, x1, sinPhase, gui.Speed, "sin"),
 			 shake(y1, y2, sinPhase, gui.Speed, "cos"))
